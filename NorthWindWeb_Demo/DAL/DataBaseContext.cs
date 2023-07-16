9e24b4e067ec2c5231e7a408e3cb6be65296c625
DESKTOP-CD9LL7E\1993a <1993attitude@gmail.com>
2023-07-16 18:31:28 +0800

    build employee relevant table

--- NorthWindWeb_Demo/DAL/DataBaseContext.cs
+++ NorthWindWeb_Demo/DAL/DataBaseContext.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NorthWindWeb_Demo.DAL.Entity;
 
 namespace NorthWindWeb_Demo.DAL
 {
     public class DataBaseContext : DbContext
     {
-        protected DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
         { }
 
+        public DbSet<EntityEmployees> Employees { get; set; }
+        public DbSet<EntityEmployeeTerritories> EmployeeTerritories { get; set; }
+        public DbSet<EntityTerritories> Territories { get; set; }
+        public DbSet<EntityRegion> Region { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EntityEmployeeTerritories>().HasKey(table => new { table.TerritoryID, table.EmployeeID });
+        }
     }
 }
--- NorthWindWeb_Demo/Program.cs
+++ NorthWindWeb_Demo/Program.cs
@@ -1,9 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using NorthWindWeb_Demo.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+//builder.Services.AddSqlServer
+var t = builder.Configuration.GetConnectionString("DefaultConnectionString");
+builder.Services.AddDbContext<DataBaseContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
 var app = builder.Build();
+
+app.MapGet("/", async (DataBaseContext db) =>
+{
+    var result = await db.Employees.ToListAsync();
+    return result;
+});
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
