11fd7f5c4f3fa808887af63500b661e796a91066
DESKTOP-CD9LL7E\1993a <1993attitude@gmail.com>
2023-07-16 17:30:51 +0800

    1.create ef dbcontext. 2.test push to remote

---
+++ NorthWindWeb_Demo/DAL/DataBaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NorthWindWeb_Demo.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        protected DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        { }
+
+
+    }
+}
